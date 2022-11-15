0e9acc0054766332b8bd121f4227b18a306a1ae0
Sukhankin Vladimir <vladimir.sukhankin@redlab.dev>
2022-11-15 07:58:59 +0400

    добавление проекта для теста и самих тестов (#14)
    Co-authored-by: Суханкин Владимир <v.sukhankin@kmd24.com>

---
+++ TestProject/BankAccountBuilder.cs
@@ -0,0 +1,33 @@
+﻿using ProjectForTest;
+
+namespace TestProject;
+
+public class BankAccountBuilder
+{
+    private BankAccount _bankAccount = new BankAccount();
+
+    public BankAccountBuilder WithStartBalance(double balance)
+    {
+        _bankAccount.Balance = balance;
+        return this;
+    }
+
+    public BankAccountBuilder WithAge(int age)
+    {
+        _bankAccount.AgeCustomer = age;
+        return this;
+    }
+    
+    public BankAccountBuilder WithName(string name)
+    {
+        _bankAccount.NameCustomer = name;
+        return this;
+    }
+
+    public BankAccount Create()
+    {
+        return _bankAccount;
+    }
+
+
+}
---
+++ TestProject/UnitTest1.cs
@@ -0,0 +1,103 @@
+using System;
+using Xunit;
+using FluentAssertions;
+
+namespace TestProject;
+
+public class BanksTests
+{
+    public BankAccountBuilder BankAccountBuilder = new BankAccountBuilder();
+
+    [Fact]
+    public void Debit_WithValid_Amount_NotUpdatesBalance()
+    {
+        //Arrange
+        var bankAccount = BankAccountBuilder
+            .WithName("Pol")
+            .WithStartBalance(0)
+            .WithAge(20)
+            .Create();
+
+        //Act
+        var act = () => bankAccount.Debit(10);
+
+        //Assert
+        act.Should().Throw<ArgumentOutOfRangeException>()
+            .WithParameterName("The amount is more than the balance");
+    }
+
+    [Fact]
+    public void Age_Less_Than_18()
+    {
+        //Arrange
+        var bankAccount = BankAccountBuilder
+            .WithName("Pols")
+            .WithStartBalance(1)
+            .WithAge(17)
+            .Create();
+
+        //Act
+        var isCreatedAccount = bankAccount.IsCreateAccount();
+
+        //Assert
+        isCreatedAccount.Should().BeFalse();
+    }
+    
+    [Fact]
+    public void Debit_WithInvalidValid_Amount_NotUpdatesBalance()
+    {
+        //Arrange
+        var bankAccount = BankAccountBuilder
+            .WithName("Poli")
+            .WithStartBalance(100)
+            .WithAge(20)
+            .Create();
+
+        //Act
+        var act = () => bankAccount.Debit(-10);
+
+        //Assert
+        act.Should().Throw<ArgumentOutOfRangeException>()
+            .WithParameterName("The amount must be greater than 0");
+    }
+    
+    [Fact]
+    public void Debit_WithValid_Amount_UpdatesBalance()
+    {
+        //Arrange
+        var bankAccount = BankAccountBuilder
+            .WithName("Poli")
+            .WithStartBalance(100)
+            .WithAge(20)
+            .Create();
+
+        //Act
+        bankAccount.Debit(10.123);
+
+        //Assert
+        bankAccount.Balance.Should().BePositive();
+        bankAccount.Balance.Should().BeLessThan(100);
+        bankAccount.Balance.Should().BeApproximately(90, 10.123);
+        
+            
+    }
+    
+    [Fact]
+    public void Credit_WithValid_Amount_UpdatesBalance()
+    {
+        //Arrange
+        var bankAccount = BankAccountBuilder
+            .WithName("Poli")
+            .WithStartBalance(100)
+            .WithAge(20)
+            .Create();
+
+        //Act
+        bankAccount.Credit(10.10);
+
+        //Assert
+        bankAccount.Balance.Should().BePositive();
+        bankAccount.Balance.Should().BeGreaterThan(100);
+        bankAccount.Balance.Should().BeApproximately(100, 10.10);
+    }
+}
